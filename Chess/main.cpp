f2e59226f5f2251f7a534e1e724a69218d143107
Ryan Yao <theryanyao@gmail.com>
2022-05-12 09:20:23 -0700

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Ryan Yao on 5/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

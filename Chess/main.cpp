bdd683fb4d967b5eabcaa4d0e762e93df0ce786a
Ryan Yao <theryanyao@gmail.com>
2022-05-24 12:20:30 -0700

    fixed rook constructor

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -55,7 +55,7 @@ public:
 
 class Rook: public Piece{
 public:
-    Rook(int side_in) : Piece(side_in, false){}
+    Rook(int side_in, int row_in, int col_in) : Piece(side_in, false, row_in, col_in){}
     int move(int i, int j) override {
         return 0;
     }
